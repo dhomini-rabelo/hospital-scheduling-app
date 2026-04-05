1d3d0c6f8d32d30da5330c391e7d150f428596e9
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2026-04-04 23:50:13 -0300

    minor typing fix

--- backend/src/domain/utils/team-member.ts
+++ backend/src/domain/utils/team-member.ts
@@ -1,12 +1,13 @@
 import {
   Profession,
   PROFESSION_SPECIALTIES,
+  Specialty,
 } from '@/domain/entities/team-member'
 import { ValidationError } from '@/modules/domain/errors'
 
 export function validateSpecialtyForProfession(
   profession: Profession,
-  specialty: string,
+  specialty: Specialty,
 ) {
   if (!PROFESSION_SPECIALTIES[profession].includes(specialty)) {
     throw new ValidationError({
